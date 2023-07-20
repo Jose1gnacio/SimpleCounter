00e380f0b8108d1c2936abece545f075cfdf9568
José Alvarez <104277952+Jose1gnacio@users.noreply.github.com>
2023-07-20 01:56:06 +0000

    finish

--- src/js/component/timeOut.jsx
+++ src/js/component/timeOut.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import PropTypes from "prop-types";
+import PropTypes, { number } from "prop-types";
 
 //create your first component
 
@@ -9,56 +9,24 @@ const TimeOut = (props) => {
         <div>
             <div className="timerOut">
                 <div className="timerOutIcon">
-                    <strong><i class="fa-solid fa-hourglass-start fa-2xl"></i></strong>
+                    <strong><i className="fa-solid fa-hourglass-start fa-2xl"></i></strong>
                 </div>
                 <div className="number2" id="position1"><strong>0</strong></div>
                 <div className="number2" id="position1"><strong>0</strong></div>
                 <div className="point2"><strong>:</strong></div>
                 <div className="number2" ><strong>0</strong></div>
-                <div className="number2" ><strong>0</strong></div>
+                <div className="number2" ><strong>{props.number9}</strong></div>
                 <div className="point2"><strong>:</strong></div>
                 <div className="number2" ><strong>{props.number8}</strong></div>
                 <div className="number2" ><strong>{props.number7}</strong></div>
             </div>
-
-            <div className="controlTimerOut">
-                <button type="button" className="btnSetTime btn btn-warning" data-bs-toggle="modal" data-bs-target="#exampleModal"><strong>SET TIME</strong></button>
-
-                <div className="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                    <div className="modal-dialog">
-                        <div className="modal-content">
-                            <div className="modal-body">
-                                <form>
-                                    <div className="mb-3">
-                                        <label for="message-text" className="col-form-label">Enter the time</label>
-                                        <input className="form-control" id="message-text" />
-                                    </div>
-                                    <div className="mb-3">
-                                        <label for="message-text" className="col-form-label">Select time unit</label>
-                                        <select className="form-select" id="time-unit">
-                                            <option value="seconds">Seconds</option>
-                                            <option value="minutes">Minutes</option>
-                                            <option value="hours">Hours</option>
-                                        </select>
-                                    </div>
-                                </form>
-                            </div>
-                            <div className="modal-footer">
-                                <button type="button" className="btn btn-dark" data-bs-dismiss="modal">Close</button>
-                                <input type="submit" className="btn btn-warning" value="Submit" />
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
         </div>
-
     )
 };
 
 TimeOut.propTypes = {
-	number1: PropTypes.number,
-	number2: PropTypes.number,
+    number1: PropTypes.number,
+    number2: PropTypes.number,
 };
 
 
--- src/js/index.js
+++ src/js/index.js
@@ -11,15 +11,16 @@ import "../styles/index.css";
 const CounterTime = (props) => {
 
 	return (
-		<>			
+		<>
 			<SimpleCounter {...props} />
-			<TimeOut {...props} />				
+			<TimeOut {...props} />
 
 		</>
 	)
 };
 
-let number = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9,];
+let number = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
 let a = 0;
 let e = 0;
 let i = 0;
@@ -27,28 +28,36 @@ let o = 0;
 let u = 0;
 let z = 0;
 let myInterval = null;
-let b = 9;
-let c = 3;
 
-
+let numberTwo = [10, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+let b = 11;
+let c = 6;
+let d = 4
 function counter() {
 	/* console.log(number[i]); */
 	a++;
-	if (number[a] === 9) {
+	if (number[a] === 10) {
 		a = 0;
 		e++;
 		if (number[e] === 6) {
 			e = 0;
-			i++			
+			i++
 		};
 	};
+
 	b--;
-	if (number[b] === 0) {
-		b = 9;
+	if (numberTwo[b] === 10) {
+		b = 10;
 		c--;
+		if (c === 0) {
+			d--;
+			c = 6
+		}
+
 	};
 
-	ReactDOM.render(<CounterTime playButton={playTimer} pauseButton={pauseTimer} resetButton={resetTimer} number1={number[a]} number2={number[e]} number3={number[i]} number4={number[o]} number5={number[u]} number6={number[z]} number7={number[b]} number8={number[c]} />, document.getElementById("counterTime"));
+
+	ReactDOM.render(<CounterTime playButton={playTimer} pauseButton={pauseTimer} resetButton={resetTimer} number1={number[a]} number2={number[e]} number3={number[i]} number4={number[o]} number5={number[u]} number6={number[z]} number7={numberTwo[b]} number8={numberTwo[c]} number9={numberTwo[d]} />, document.getElementById("counterTime"));
 };
 
 
@@ -71,6 +80,9 @@ function resetTimer() {
 	o = 0;
 	u = 0;
 	z = 0;
+	b = 11;
+	c = 6;
+	d = 4;
 	if (!myInterval) {
 		myInterval = setInterval(counter, 1000);
 	};
